a0db50dca09af0110facf10589f18e4033b26b23
HMDuran <hanahmae.duran@gmail.com>
2025-07-07 16:35:28 +0800

    update: about me details

--- src/data/AboutDetails.js
+++ src/data/AboutDetails.js
@@ -3,7 +3,7 @@ const AboutDetails = [
     bio: (
       <>
         <p>
-          Hi! I'm <strong>Hanah Mae Duran</strong>, a <em>4th-year Bachelor of Science in Computer Science</em> student at <strong>Cavite State University – Imus Campus</strong>. I’m passionate about <strong>technology</strong> and <strong>software development</strong>, and always eager to turn ideas into real-world solutions through code.
+          Hi! I'm <strong>Hanah Mae Duran</strong>, a <em>3rd-year Bachelor of Science in Computer Science</em> student at <strong>Cavite State University – Imus Campus</strong>. I’m passionate about <strong>technology</strong> and <strong>software development</strong>, and always eager to turn ideas into real-world solutions through code.
         </p>
         <p>
           I enjoy <strong>learning</strong>, <strong>solving problems</strong>, and working on <em>innovative projects</em> that create meaningful impact — whether independently or as part of a team. I’ve worked with tools and technologies such as <strong>HTML</strong>, <strong>CSS</strong>, <strong>JavaScript</strong>, <strong>ReactJS</strong>, <strong>NodeJS</strong>, and <strong>MySQL</strong>. I also enjoy using <strong>Git</strong> and <strong>GitHub</strong> to manage and share code efficiently.
